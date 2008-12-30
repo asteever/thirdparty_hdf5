87fce6eb73e216e23d2b4921af2957f5ac26593a
brtnfld <brtnfld@dab4d1a6-ed17-0410-a064-d1ae371a2980>
2008-12-30 21:52:10 +0000

    Initial implementation of
     h5oclose_f, h5oget_info_by_name_f

--- branches/F2003/fortran/src/H5Off.f90
+++ branches/F2003/fortran/src/H5Off.f90
@@ -13,20 +13,20 @@
 !
 !
 ! COPYRIGHT
-!  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-!  Copyright by The HDF Group.                                               *
-!  Copyright by the Board of Trustees of the University of Illinois.         *
-!  All rights reserved.                                                      *
-!  *
-!  This file is part of HDF5.  The full HDF5 copyright notice, including     *
-!  terms governing use, modification, and redistribution, is contained in    *
-!  the files COPYING and Copyright.html.  COPYING can be found at the root   *
-!  of the source code distribution tree; Copyright.html can be found at the  *
-!  root level of an installed copy of the electronic HDF5 document set and   *
-!  is linked from the top-level documents page.  It can also be found at     *
-!  http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have          *
-!  access to either file, you may request a copy from help@hdfgroup.org.     *
-!  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+! * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+!   Copyright by The HDF Group.                                               *
+!   Copyright by the Board of Trustees of the University of Illinois.         *
+!   All rights reserved.                                                      *
+!                                                                             *
+!   This file is part of HDF5.  The full HDF5 copyright notice, including     *
+!   terms governing use, modification, and redistribution, is contained in    *
+!   the files COPYING and Copyright.html.  COPYING can be found at the root   *
+!   of the source code distribution tree; Copyright.html can be found at the  *
+!   root level of an installed copy of the electronic HDF5 document set and   *
+!   is linked from the top-level documents page.  It can also be found at     *
+!   http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have          *
+!   access to either file, you may request a copy from help@hdfgroup.org.     *
+! * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 !
 ! NOTES
 !                         *** IMPORTANT ***
@@ -65,7 +65,6 @@ CONTAINS
 !  April 21, 2008
 !
 ! SOURCE
-
   SUBROUTINE h5olink_f(object_id, new_loc_id, new_link_name, hdferr, lcpl_id, lapl_id)
     IMPLICIT NONE
     INTEGER(HID_T), INTENT(IN) :: object_id  ! Object to be linked
@@ -133,7 +132,6 @@ CONTAINS
 !  M.S. Breitenfeld
 !  April 18, 2008
 ! SOURCE
-
   SUBROUTINE h5oopen_f(loc_id, name, obj_id, hdferr, lapl_id)
     IMPLICIT NONE
     INTEGER(HID_T), INTENT(IN) :: loc_id  ! File or group identifier
@@ -169,5 +167,177 @@ CONTAINS
     hdferr = h5oopen_c(loc_id, name, namelen, lapl_id_default, obj_id)
 
   END SUBROUTINE h5oopen_f
+!
+!****s* H5O/h5oclose_f
+!
+! NAME
+!  h5oclose_f
+!
+! PURPOSE
+!  Closes an object in an HDF5 file.
+!
+! INPUTS
+!  object_id     - Object identifier
+! OUTPUTS
+!  hdferr 	 - Returns 0 if successful and -1 if fails
+!
+! AUTHOR
+!  M.S. Breitenfeld
+!  December 17, 2008
+!
+! SOURCE
+  SUBROUTINE h5oclose_f(object_id, hdferr)
+    IMPLICIT NONE
+    INTEGER(HID_T), INTENT(IN)  :: object_id
+    INTEGER,        INTENT(OUT) :: hdferr
+!*****
+    INTERFACE
+       INTEGER FUNCTION h5oclose_c(object_id)
+         USE H5GLOBAL
+         !DEC$ IF DEFINED(HDF5F90_WINDOWS)
+         !DEC$ OBJECTIBUTES C,reference,decorate,alias:'H5OCLOSE_C'::h5oclose_c
+         !DEC$ ENDIF
+         INTEGER(HID_T), INTENT(IN) :: object_id
+       END FUNCTION h5oclose_c
+    END INTERFACE
+
+    hdferr = h5oclose_c(object_id)
+  END SUBROUTINE h5oclose_f
+
+!!$
+!!$
+!!$TYPE space_t
+!!$   INTEGER(hsize_t) :: total ! Total space for storing object header in file
+!!$   INTEGER(hsize_t) :: meta  ! Space within header for object header metadata information
+!!$   INTEGER(hsize_t) :: mesg  ! Space within header for actual message information
+!!$   INTEGER(hsize_t) :: free  ! Free space within object header
+!!$END TYPE space_t
+!!$
+!!$TYPE mesg_t
+!!$   INTEGER(c_int64_t) :: present ! Flags to indicate presence of message type in header 
+!!$   INTEGER(c_int64_t) :: shared ! Flags to indicate message type is shared in header
+!!$ ! uint64_t present
+!!$ ! uint64_t shared
+!!$END TYPE mesg_t
+!!$
+!!$TYPE hdr_t
+!!$   INTEGER(c_int) :: version ! Version number of header format in file
+!!$   ! unsigned version                          */
+!!$   INTEGER(c_int) :: nmesgs ! Number of object header messages
+!!$   ! unsigned nmesgs 
+!!$   INTEGER(c_int) :: nchunks ! Number of object header chunks 
+!!$   ! unsigned nchunks
+!!$   INTEGER(c_int) :: flags ! Object header status flags
+!!$   ! unsigned flags
+!!$   TYPE(space_t) :: space
+!!$   TYPE(mesg_t) :: mesg
+!!$END TYPE hdr_t
+!!$
+!!$! Extra metadata storage for obj & attributes
+!!$TYPE H5_ih_info_t
+!!$    INTEGER(hsize_t) :: index_size ! btree and/or list
+!!$    INTEGER(hsize_t) :: heap_size
+!!$ END TYPE H5_ih_info_t
+!!$
+!!$TYPE meta_size_t
+!!$   TYPE(H5_ih_info_t) :: obj  ! v1/v2 B-tree & local/fractal heap for groups, B-tree for chunked datasets
+!!$   TYPE(H5_ih_info_t) :: attr ! v2 B-tree & heap for attributes
+!!$ENDTYPE meta_size_t
+!!$
+!!$
+!!$TYPE H5O_info_t
+!!$   INTEGER(HADDR_T) ::  fileno ! File number that object is located in
+!!$   ! unsigned long       fileno;
+!!$   INTEGER(HADDR_T) :: addr  ! Object address in file  
+!!$   INTEGER :: type ! Basic object type (group, dataset, etc.) 
+!!$   ! H5O_type_t          type
+!!$   INTEGER(c_int) :: rc  ! Reference count of object
+!!$   ! unsigned rc
+!!$    time_t              atime;          /* Access time                        */
+!!$    time_t              mtime;          /* Modification time                  */
+!!$    time_t              ctime;          /* Change time                        */
+!!$    time_t              btime;          /* Birth time                         */
+!!$   INTEGER(hsize_t) :: num_attrs ! # of attributes attached to object
+!!$
+!!$   TYPE(hdr_t) :: hdr
+!!$
+!!$   TYPE(meta_size_t) :: meta_size
+!!$END TYPE H5O_info_t
+
+
+!
+!****s* H5O/h5oget_info_by_name_f
+!
+! NAME
+!  h5oget_info_by_name_f
+!
+! PURPOSE
+!  Retrieves the metadata for an object, identifying the object by location and relative name.
+!
+! INPUTS
+!  loc_id 	  - File or group identifier specifying location of group in which object 
+!                   is located.
+!  name 	  - Name of group, relative to loc_id
+!
+! OUTPUTS  NOTE: In C it is defined as a structure: H5O_info_t
+!    **** NEED TO MAKE THIS DERIVED DATATYPE ****
+!  hdferr 	  - Returns 0 if successful and -1 if fails
+! OPTIONAL PARAMETERS
+!  lapl_id 	  - Link access property list
+!
+! AUTHOR
+!  M.S. Breitenfeld
+!  December 1, 2008
+!
+! SOURCE
+!!$  SUBROUTINE h5oget_info_by_name_f(loc_id, name, &
+!!$       f_corder_valid, corder, cset, data_size, hdferr, lapl_id)
+!!$    IMPLICIT NONE
+!!$    INTEGER(HID_T)  , INTENT(IN)            :: loc_id
+!!$    CHARACTER(LEN=*), INTENT(IN)            :: name
+!!$    LOGICAL         , INTENT(OUT)           :: f_corder_valid
+!!$    INTEGER         , INTENT(OUT)           :: corder
+!!$    INTEGER         , INTENT(OUT)           :: cset
+!!$    INTEGER(HSIZE_T), INTENT(OUT)           :: data_size
+!!$    INTEGER         , INTENT(OUT)           :: hdferr
+!!$    INTEGER(HID_T)  , INTENT(IN) , OPTIONAL :: lapl_id
+!!$!*****
+!!$    INTEGER         :: corder_valid
+!!$    INTEGER(SIZE_T) :: namelen
+!!$    INTEGER(HID_T)  :: lapl_id_default
+!!$    
+!!$    INTERFACE
+!!$       INTEGER FUNCTION h5oget_info_by_name_c(loc_id, name, namelen, lapl_id_default, &
+!!$            corder_valid, corder, cset, data_size)
+!!$         USE H5GLOBAL
+!!$         !DEC$ IF DEFINED(HDF5F90_WINDOWS)
+!!$         !DEC$ ATTRIBUTES C,reference,decorate,alias:'H5OGET_INFO_BY_NAME_C'::h5oget_info_by_name_c
+!!$         !DEC$ ENDIF
+!!$         INTEGER(HID_T)  , INTENT(IN)  :: loc_id
+!!$         CHARACTER(LEN=*), INTENT(IN)  :: name
+!!$         INTEGER(SIZE_T) , INTENT(IN)  :: namelen
+!!$         INTEGER(HID_T)  , INTENT(IN)  :: lapl_id_default
+!!$         INTEGER         , INTENT(OUT) :: corder_valid
+!!$         INTEGER         , INTENT(OUT) :: corder
+!!$         INTEGER         , INTENT(OUT) :: cset
+!!$         INTEGER(HSIZE_T), INTENT(OUT) :: data_size
+!!$
+!!$       END FUNCTION h5oget_info_by_name_c
+!!$    END INTERFACE
+!!$
+!!$    namelen = LEN(name)
+!!$
+!!$    lapl_id_default = H5P_DEFAULT_F
+!!$    IF(PRESENT(lapl_id)) lapl_id_default = lapl_id
+!!$
+!!$    hdferr = H5Oget_info_by_name_c(loc_id, name, namelen, lapl_id_default, &
+!!$         corder_valid, corder, cset, data_size)
+!!$
+!!$    f_corder_valid =.FALSE.
+!!$    IF (corder_valid .EQ. 1) f_corder_valid =.TRUE.
+!!$
+!!$  END SUBROUTINE H5Oget_info_by_name_f
+
 
 END MODULE H5O
+
