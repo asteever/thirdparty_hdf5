17b97523e38c53bc60c6211d89bb09b64e9b2719
brtnfld <brtnfld@dab4d1a6-ed17-0410-a064-d1ae371a2980>
2008-12-30 21:25:04 +0000

    Description:
      This file contains Fortran 90 and Fortran 2003 interfaces for H5T functions.
      It contains the same functions as H5Tff_DEPRECIATE.f90 but includes the
      Fortran 2003 functions and the interface listings. This file will be compiled
      instead of H5Tff_DEPRECIATE.f90 if Fortran 2003 functions are enabled.
    
    Added function H5Tconvert_f

---
+++ fortran/src/H5Tff_F03.f90
@@ -0,0 +1,106 @@
+!****h* ROBODoc/H5T (F03)
+!
+! NAME
+!  H5T_PROVISIONAL
+!
+! PURPOSE
+!  This file contains Fortran 90 and Fortran 2003 interfaces for H5T functions.
+!  It contains the same functions as H5Tff_DEPRECIATE.f90 but includes the
+!  Fortran 2003 functions and the interface listings. This file will be compiled
+!  instead of H5Tff_DEPRECIATE.f90 if Fortran 2003 functions are enabled.
+!
+! COPYRIGHT
+! * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+!   Copyright by The HDF Group.                                               *
+!   Copyright by the Board of Trustees of the University of Illinois.         *
+!   All rights reserved.                                                      *
+!                                                                             *
+!   This file is part of HDF5.  The full HDF5 copyright notice, including     *
+!   terms governing use, modification, and redistribution, is contained in    *
+!   the files COPYING and Copyright.html.  COPYING can be found at the root   *
+!   of the source code distribution tree; Copyright.html can be found at the  *
+!   root level of an installed copy of the electronic HDF5 document set and   *
+!   is linked from the top-level documents page.  It can also be found at     *
+!   http://hdfgroup.org/HDF5/doc/Copyright.html.  If you do not have          *
+!   access to either file, you may request a copy from help@hdfgroup.org.     *
+! * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+!
+! NOTES
+!                         *** IMPORTANT ***
+!  If you add a new H5P function you must add the function name to the
+!  Windows dll file 'hdf5_fortrandll.def' in the ROBODoc directory.
+!  This is needed for Windows based operating systems.
+!
+!*****
+
+MODULE H5T_PROVISIONAL
+
+  USE H5GLOBAL
+
+CONTAINS
+
+!****s* H5T (F03)/H5Tconvert_f
+!
+! NAME
+!  H5Tconvert_f
+!
+! PURPOSE
+!  Converts data from between specified datatypes.
+!
+! INPUTS
+!  src_id     - Identifier for the source datatype.
+!  dst_id     - Identifier for the destination datatype.
+!  nelmts     - Size of array buf.
+!  buf 	      - Array containing pre-conversion values.
+!  background - Optional background buffer.
+!  plist_id   -	Dataset transfer property list identifier.
+!
+! OUTPUTS.
+!  buf 	      - Array containing post-conversion values.
+!  hdferr     - error code:
+!                 0 on success and -1 on failure
+! AUTHOR
+!  M.S. Breitenfeld
+!  Decemember 8, 2008
+!
+! SOURCE
+  SUBROUTINE h5tconvert_f(src_id, dst_id, nelmts, buf, hdferr, background, plist_id)
+    USE ISO_C_BINDING
+    IMPLICIT NONE
+    INTEGER(HID_T) , INTENT(IN)               :: src_id
+    INTEGER(HID_T) , INTENT(IN)               :: dst_id
+    INTEGER(SIZE_T), INTENT(IN)               :: nelmts
+    TYPE(C_PTR)    , INTENT(INOUT)            :: buf
+    INTEGER        , INTENT(OUT)              :: hdferr
+    TYPE(C_PTR)    , INTENT(INOUT), OPTIONAL  :: background
+    INTEGER(HID_T) , INTENT(IN)   , OPTIONAL  :: plist_id
+!*****
+    INTEGER(HID_T) :: plist_id_c
+
+    INTERFACE
+       INTEGER FUNCTION h5tconvert_c(src_id, dst_id, nelmts, buf, background, plist_id)
+         USE ISO_C_BINDING
+         USE H5GLOBAL
+         !DEC$ IF DEFINED(HDF5F90_WINDOWS)
+         !DEC$ ATTRIBUTES C,reference,decorate,alias:'H5TCONVERT_C'::H5Tconvert_c
+         !DEC$ ENDIF
+         INTEGER(HID_T) , INTENT(IN)           :: src_id
+         INTEGER(HID_T) , INTENT(IN)           :: dst_id
+         INTEGER(SIZE_T), INTENT(IN)           :: nelmts
+         TYPE(C_PTR)                , VALUE    :: buf
+         TYPE(C_PTR)                , VALUE    :: background
+         INTEGER(HID_T) , INTENT(IN)           :: plist_id
+       END FUNCTION h5tconvert_c
+    END INTERFACE
+
+    plist_id_c = H5P_DEFAULT_F
+    IF(PRESENT(plist_id)) plist_id_c = plist_id
+
+    IF(.NOT.PRESENT(background)) background = C_NULL_PTR
+
+    hdferr = H5Tconvert_c(src_id, dst_id, nelmts, buf, background, plist_id_c)
+
+  END SUBROUTINE h5tconvert_f
+
+END MODULE H5T_PROVISIONAL
+
